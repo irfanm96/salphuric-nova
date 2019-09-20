cc78d47e39f36f0e0d4e0e3da4f4486e20b1f386
Mohamed Fawzan <fawzanxyz@gmail.com>
2019-09-20 10:44:44 +0530

    Initial commit with base

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'admin']);
+        $role = Role::create(['name' => 'super-admin']);
+        $role = Role::create(['name' => 'user']);
+        $role = Role::create(['name' => 'manager']);
+
+        User::create([
+            'name' => 'Mohamed Fawzan',
+            'email' => 'fawzanm@gmail.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now(),
+        ])->assignRole('super-admin');
+
+
+        User::create([
+            'name' => 'Mohamed Irfan',
+            'email' => 'irfanmm96@gmail.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now(),
+        ])->assignRole('super-admin');
+
+        factory(User::class, 100)->create();
+    }
+}
