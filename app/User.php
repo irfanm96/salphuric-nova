3a908156cfb16b6521d583541b5832d4a3f404d0
Mohamed Fawzan <fawzanxyz@gmail.com>
2019-09-21 16:45:46 +0530

    Emergency Updates

--- app/User.php
+++ app/User.php
@@ -5,13 +5,14 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Nova\Actions\Actionable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
     use HasRoles;
-
+    use Actionable;
 
     /**
      * The attributes that are mass assignable.
