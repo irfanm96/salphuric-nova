e4cf7c996aaed9da3ff5ec4cafa47f7c972f4da3
irfanm96 <e15138@ce.pdn.ac.lk>
2019-09-21 12:33:10 +0530

    added product resource in nova

---
+++ app/Nova/Products.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Products extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Product';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        $fields = [];
+        $fields[] = ID::make()->sortable()->hideFromIndex();
+        $fields[] = Text::make('Name')->sortable();
+        $fields[] = Text::make('Slug')->hideFromIndex();
+        $fields[] = Text::make('Description');
+        $fields[] = Text::make('Provider');
+        $fields[] = Number::make('Quantity');
+        $fields[] = Currency::make('Price Per Unit')->format('%.2n')->rules('required');
+        $fields[] = Select::make('Frequency')->options([
+            'per_unit' => 'Per Unit',
+            'daily' => 'Daily',
+            'monthly' => 'monthly',
+            'annually' => 'annually',
+        ])->rules('required');
+
+        return $fields;
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -1,10 +1,10 @@
 <dropdown-trigger class="h-9 flex items-center" slot-scope="{toggle}" :handle-click="toggle">
-    <div class="flex-no-shrink ml-auto"><a
-            href="/dashboard/resources/users/new?viaResource=&amp;viaResourceId=&amp;viaRelationship="
-            class="btn btn-default btn-primary" dusk="create-button">
-            New Project
-        </a></div>
-
+    <button class="btn btn-primary bg-blue-300 hover:bg-blue-400 text-white-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+        </svg>
+        <span>New Project</span>
+    </button>
     @isset($user->email)
         <img
             src="https://secure.gravatar.com/avatar/{{ md5($user->email) }}?size=512"
