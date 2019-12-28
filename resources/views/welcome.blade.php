5d89062fb71cfbff84448b66abc1a28ae2bf6c8b
irfanm96 <e15138@ce.pdn.ac.lk>
2019-12-28 09:25:34 +0530

    extracted header and footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,36 +79,7 @@
 <!--==========================
   Header
 ============================-->
-<header id="header">
-    <div class="container-fluid">
-
-        <div id="logo" class="pull-left">
-            {{--<h1><a href="/themes/bizpage/#intro" class="scrollto">Salfade</a></h1>--}}
-            {{--<!-- Uncomment below if you prefer to use an image logo -->--}}
-            <a href="/"><img src="/img/logo.png" alt="" title=""/></a>
-        </div>
-
-        <nav id="nav-menu-container">
-            <ul class="nav-menu">
-                <li class="menu-active"><a href="#intro">Home</a></li>
-                <li><a href="#about">About Us</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                {{--<li><a href="#team">Team</a></li>--}}
-                {{--<li class="menu-has-children"><a href="">Drop Down</a>--}}
-                {{--<ul>--}}
-                {{--<li><a href="#">Drop Down 1</a></li>--}}
-                {{--<li><a href="#">Drop Down 3</a></li>--}}
-                {{--<li><a href="#">Drop Down 4</a></li>--}}
-                {{--<li><a href="#">Drop Down 5</a></li>--}}
-                {{--</ul>--}}
-                {{--</li>--}}
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav><!-- #nav-menu-container -->
-    </div>
-</header><!-- #header -->
-
+@include('layouts.header')
 <!--==========================
   Intro Section
 ============================-->
@@ -974,77 +945,7 @@
 <!--==========================
   Footer
 ============================-->
-<footer id="footer">
-    <div class="footer-top">
-        <div class="container">
-            <div class="row">
-
-                {{--<div class="col-lg-3 col-md-6 footer-info">--}}
-                {{--<h3>BizPage</h3>--}}
-                {{--<p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita--}}
-                {{--valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet--}}
-                {{--proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>--}}
-                {{--</div>--}}
-
-                {{--<div class="col-lg-3 col-md-6 footer-links">--}}
-                {{--<h4>Useful Links</h4>--}}
-                {{--<ul>--}}
-                {{--<li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>--}}
-                {{--<li><i class="ion-ios-arrow-right"></i> <a href="#">About us</a></li>--}}
-                {{--<li><i class="ion-ios-arrow-right"></i> <a href="#">Services</a></li>--}}
-                {{--<li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>--}}
-                {{--<li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>--}}
-                {{--</ul>--}}
-                {{--</div>--}}
-
-                {{--<div class="col-lg-3 col-md-6 footer-contact">--}}
-                {{--<h4>Contact Us</h4>--}}
-                {{--<p>--}}
-                {{--A108 Adam Street <br>--}}
-                {{--New York, NY 535022<br>--}}
-                {{--United States <br>--}}
-                {{--<strong>Phone:</strong> +1 5589 55488 55<br>--}}
-                {{--<strong>Email:</strong> info@example.com<br>--}}
-                {{--</p>--}}
-
-                <div class="social-links">
-                    {{--<a href="#" class="twitter"><i class="fa fa-twitter"></i></a>--}}
-                    <a href="http://facebook.com/salfade" class="facebook"><i class="fa fa-facebook"></i></a>
-                    {{--<a href="#" class="instagram"><i class="fa fa-instagram"></i></a>--}}
-                    {{--<a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>--}}
-                    {{--<a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>--}}
-                </div>
-
-                {{--</div>--}}
-
-                {{--<div class="col-lg-3 col-md-6 footer-newsletter">--}}
-                {{--<h4>Our Newsletter</h4>--}}
-                {{--<p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum--}}
-                {{--dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>--}}
-                {{--<form action="" method="post">--}}
-                {{--<input type="email" name="email"><input type="submit" value="Subscribe">--}}
-                {{--</form>--}}
-                {{--</div>--}}
-
-            </div>
-        </div>
-    </div>
-
-    {{--<div class="container">--}}
-    {{--<div class="copyright">--}}
-    {{--&copy; Copyright <strong>BizPage</strong>. All Rights Reserved--}}
-    {{--</div>--}}
-    {{--<div class="credits">--}}
-    {{--<!----}}
-    {{--All the links in the footer should remain intact.--}}
-    {{--You can delete the links only if you purchased the pro version.--}}
-    {{--Licensing information: https://bootstrapmade.com/license/--}}
-    {{--Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage--}}
-    {{---->--}}
-    {{--Best <a href="https://bootstrapmade.com/">Bootstrap Templates</a> by BootstrapMade--}}
-    {{--</div>--}}
-    {{--</div>--}}
-</footer><!-- #footer -->
+@include('layouts.footer')
 
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
